c1a576be1753f603ec11dba7872256181a63a11c
SNEH PATEL <139197122+Sneh4201@users.noreply.github.com>
2024-12-26 12:20:06 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'sneh and maitri',
+    author_email= 'sneh0401@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
